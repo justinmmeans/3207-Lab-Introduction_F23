5549c4bdfd7cefc54d28f3aee371d873b9c81247
justinmmeans <justin.means@temple.edu>
2023-08-30 20:56:19 -0400

    Revision of previous commit: Changed max input

--- random.c
+++ random.c
@@ -1,13 +1,15 @@
 //Justin Means
 //Function program to generate a random character for print_random.c
-
+//hello
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-char randchar(){
+char randchar()
+{
 
     char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-	return alphabet[rand() % 25 + 0];
+	return alphabet[rand() % 26];
+
 }
